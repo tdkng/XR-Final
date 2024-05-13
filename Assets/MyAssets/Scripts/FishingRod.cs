aec5fe084bb94651a20f7b08c2ef9c0d4ffb5ae5
timothy <timothydknguyen@berkeley.edu>
2024-05-13 13:42:26 -0700

    added scene, fishrod

---
+++ Assets/MyAssets/Scripts/FishingRod.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FishingRod : MonoBehaviour
+{
+    public GameObject bobberPrefab; // Prefab for the bobber
+    public Transform castPoint; // Point from which the bobber is spawned
+    public float castForce = 10f; // Force applied to the bobber when casting
+    public LayerMask waterLayer; // Layer mask for the water surface
+    public float reelSpeed = 5f; // Speed at which the bobber is reeled in
+
+    private GameObject bobberInstance; // Reference to the spawned bobber
+    private bool isCasting = false; // Flag to track if the rod is currently casting
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Gamepad.current != null && Gamepad.current.buttonSouth.wasPressedThisFrame)
+        {
+            if (!isCasting)
+            {
+                Cast();
+            }
+            else
+            {
+                Reel();
+            }
+        }
+    }
+
+    private void Cast()
+    {
+        isCasting = true;
+        bobberInstance = Instantiate(bobberPrefab, castPoint.position, Quaternion.identity);
+        Rigidbody bobberRb = bobberInstance.GetComponent<Rigidbody>();
+        bobberRb.AddForce(transform.forward * castForce, ForceMode.Impulse);
+    }
+
+    private void Reel()
+    {
+        if (bobberInstance != null)
+        {
+            Transform bobberTransform = bobberInstance.transform;
+            bobberTransform.position = Vector3.MoveTowards(bobberTransform.position, transform.position, reelSpeed * Time.deltaTime);
+
+            if (bobberTransform.position == transform.position)
+            {
+                Destroy(bobberInstance);
+                isCasting = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == waterLayer)
+        {
+            // Pond script logic to spawn fish when bobber touches water
+            // Assuming the Pond script has a method to handle fish spawning
+            Pond pond = other.GetComponent<Pond>();
+            if (pond != null)
+            {
+                pond.SpawnFish();
+            }
+        }
+    }
+}
---
+++ Assets/MyAssets/Scripts/Pond.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pond : MonoBehaviour
+{
+
+    public GameObject fishPrefab; // Prefab for the fish
+    public float minSpawnDelay = 5f; // Minimum delay between fish spawns
+    public float maxSpawnDelay = 10f; // Maximum delay between fish spawns
+
+    private void Start()
+    {
+        // Start spawning fish at random intervals
+        Invoke("SpawnFish", Random.Range(minSpawnDelay, maxSpawnDelay));
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SpawnFish()
+    {
+        // Spawn a fish at a random position within the pond collider
+        Vector3 spawnPosition = GetRandomSpawnPosition();
+        Instantiate(fishPrefab, spawnPosition, Quaternion.identity);
+
+        // Schedule the next fish spawn
+        Invoke("SpawnFish", Random.Range(minSpawnDelay, maxSpawnDelay));
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        // Get a random point within the bounds of the pond collider
+        Collider collider = GetComponent<Collider>();
+        Vector3 center = collider.bounds.center;
+        Vector3 extents = collider.bounds.extents;
+        float x = Random.Range(center.x - extents.x, center.x + extents.x);
+        float z = Random.Range(center.z - extents.z, center.z + extents.z);
+        return new Vector3(x, center.y, z);
+    }
+}
